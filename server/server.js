6b3875ebd4cdb322d0ce5215785faf4a3572047d
Haricharan Konduri <hkonduri@ceredox.com>
2018-01-17 08:11:34 +0530

    Add GET todos

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
+ 
 
     todo.save().then((doc) => {
         res.send(doc);
@@ -29,7 +30,13 @@ app.post('/todos', (req, res) => {
     });
 
 });
-
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+})
 
 app.listen(3000, () => {
     console.log(
