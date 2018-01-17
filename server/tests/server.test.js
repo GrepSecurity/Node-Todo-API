8e4c4c224391003bd8bd2a6f29c0be78b2601b87
Haricharan Konduri <hkonduri@ceredox.com>
2018-01-17 08:27:32 +0530

    Added tests for Routes GET todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,17 +10,25 @@ const {
 } = require('./../models/todo');
 
 
+const todos = [{
+        text: 'First test todo'
+    },
+    {
+        text: 'Second test todo'
+    }
+];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then((todos) => done());
 });
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
         var text = 'test todo text';
         request(app).post('/todos')
-            .send({
-                text
-            })
+            .send({text})
             .expect(200)
             .expect((res) => {
                 expect(res.body.text).toBe(text);
@@ -30,7 +38,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -53,7 +61,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => {
                     done(e)
@@ -62,4 +70,16 @@ describe('POST /todos', () => {
 
     });
 
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
 });
