659dace2eb3c53ae1a66b7346e7d6b3c7adf82b4
Haricharan Konduri <hkonduri@ceredox.com>
2018-01-18 07:42:38 +0530

    Added Tests for GET /Todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,6 +1,9 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const {
+    ObjectID
+} = require('mongodb');
 
 const {
     app
@@ -11,9 +14,11 @@ const {
 
 
 const todos = [{
+        _id: new ObjectID(),
         text: 'First test todo'
     },
     {
+        _id: new ObjectID(),
         text: 'Second test todo'
     }
 ];
@@ -28,7 +33,9 @@ describe('POST /todos', () => {
     it('should create a new todo', (done) => {
         var text = 'test todo text';
         request(app).post('/todos')
-            .send({text})
+            .send({
+                text
+            })
             .expect(200)
             .expect((res) => {
                 expect(res.body.text).toBe(text);
@@ -38,7 +45,9 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find({text}).then((todos) => {
+                Todo.find({
+                    text
+                }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -80,6 +89,35 @@ describe('GET /todos', () => {
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return a 404 if todo is not a valid document', (done) => {
+        var hexId = new ObjectID().toHexString();
+
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return a 404 for non-object ids', (done) => {
+
+
+        request(app)
+            .get(`/todos/122333`)
+            .expect(404)
             .end(done);
     });
 });
