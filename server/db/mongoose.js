0dfae66581103b586bc4f724c1d0fa69a7143bfd
Haricharan Konduri <hkonduri@ceredox.com>
2018-01-18 08:36:22 +0530

    Modified Mongoose config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,14 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+var username = process.env.DB_USERNAME;
+var password = process.env.DB_PASSWORD;
+console.log(username);
+console.log(password);
+mongoose.connect("mongodb://" + username + ":" + password + "@ds155577.mlab.com:55577/todoapp-nodejs" || 'mongodb://localhost:27017/TodoApp', {
+    useMongoClient: true
+});
 module.exports = {
     mongoose
 };
